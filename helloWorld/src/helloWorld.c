3f7373690b5956209bde35f7b3787640c305643c
maga <maga@192.168.0.3>
2020-03-31 18:40:48 -0300

    Hi! I'm a commit

---
+++ helloWorld/src/helloWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : helloWorld.c
+ Author      : Maga
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	pritf("Hello world!");
+	return EXIT_SUCCESS;
+}
