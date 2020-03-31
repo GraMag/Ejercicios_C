1f35aae2dbdfd38cfbec14e4ed55a8f6fdd9c8f9
reyna <reyna@192.168.0.3>
2020-03-31 18:25:08 -0300

    Esto es un comentario / commit

---
+++ Ejercicios/src/Ejercicios.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Ejercicios.c
+ Author      : Maga
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Holi"); /* prints Holi */
+	return EXIT_SUCCESS;
+}
